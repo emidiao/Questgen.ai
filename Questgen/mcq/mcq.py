12531ed5a28c9cd5364e1852d38ea57c8ed7e0c0
Emiliano S <emiliano.schiavon@gmail.com>
2020-10-09 11:46:48 +0200

    Update mcq.py

--- Questgen/mcq/mcq.py
+++ Questgen/mcq/mcq.py
@@ -148,10 +148,10 @@ def get_nouns_multipartite(text):
     out = []
 
     extractor = pke.unsupervised.MultipartiteRank()
-    extractor.load_document(input=text, language='en')
+    extractor.load_document(input=text, language='it')
     pos = {'PROPN', 'NOUN'}
     stoplist = list(string.punctuation)
-    stoplist += stopwords.words('english')
+    stoplist += stopwords.words('italian')
     extractor.candidate_selection(pos=pos, stoplist=stoplist)
     # 4. build the Multipartite graph and rank candidates using random walk,
     #    alpha controls the weight adjustment mechanism, see TopicRank for

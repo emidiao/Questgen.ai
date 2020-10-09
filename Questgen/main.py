a9abeced475ae3ee5a02b76887825ce11996b182
Emiliano S <emiliano.schiavon@gmail.com>
2020-10-09 11:57:31 +0200

    Update main.py

--- Questgen/main.py
+++ Questgen/main.py
@@ -45,7 +45,7 @@ class QGen:
         # model.eval()
         self.device = device
         self.model = model
-        self.nlp = spacy.load('it_core_news_lg')
+        self.nlp = spacy.load('it')
 
         self.s2v = Sense2Vec().from_disk('s2v_old')
 
